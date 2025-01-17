0cc85f1cf08c4b558ea69d93451cd32979dad687
Ken Miyashita <kmiyashita23@gmail.com>
2025-01-17 21:06:53 +0900

    chore: トップページを作りかけ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,7 @@ export default function Home() {
   return (
     <Container>
       <Paper elevation={3} sx={{ p: 5 }}>
-        <h1>Welcome to Material UI!</h1>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox defaultChecked />}
-            label="Label"
-          />
-          <FormControlLabel required control={<Checkbox />} label="Required" />
-          <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
-        </FormGroup>
-        <hr />
-        <Button variant="contained" color="primary">
-          ボタン
-        </Button>
+        <h1>レース一覧（デバッグ用）</h1>
         <Link href="/race/25SP">2025 Spring</Link>
       </Paper>
     </Container>
