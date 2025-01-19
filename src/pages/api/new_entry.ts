5048cf19c8f35832e292c46885779c87aa1948fa
Ken Miyashita <kmiyashita23@gmail.com>
2025-01-19 22:21:17 +0900

    feat: MySQLにデータ追加。まだエラー WIP

--- src/pages/api/new_entry.ts
+++ src/pages/api/new_entry.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
 import prisma from "../../lib/prisma";
 
 // POST /api/new_entry
@@ -11,12 +12,53 @@ export default async function handle(
   console.log(raceId);
   console.log(formData);
 
-  //   const result = await prisma.post.create({
-  //     data: {
-  //       title: title,
-  //       content: content,
-  //       author: { connect: { email: authorEmail } },
-  //     },
-  //   });
-  return res.status(201).json(true);
+  const result = await prisma.team.create({
+    data: extractTeamFromFormData(formData, raceId),
+  });
+  return res.status(201).json(result);
+}
+
+function extractTeamFromFormData(
+  formData: any,
+  raceId: number
+): Prisma.TeamCreateInput {
+  return {
+    sailNumber: formData.sailNumber,
+    country: formData.country,
+    boatName: formData.boatName,
+    boatWeight: formData.boatWeight,
+    fleet: formData.fleet,
+    place: formData.place,
+    message: formData.message,
+    miscInJson: formData.miscInJson,
+    persons: {
+      create: [
+        extractPersonFromFormData(formData, "skipper"),
+        extractPersonFromFormData(formData, "crew1"),
+        extractPersonFromFormData(formData, "crew2"),
+      ],
+    },
+    race: { connect: { id: raceId } },
+  };
+}
+
+function extractPersonFromFormData(formData: any, role: string): any {
+  const birtyDayString = formData[`${role}_birdyDay`];
+  const birthDay = birtyDayString ? new Date(birtyDayString) : undefined;
+  return {
+    lastName: formData[`${role}_lastName`],
+    firstName: formData[`${role}_firstName`],
+    lastNameRomaji: formData[`${role}_lastNameRomaji`],
+    firstNameRomaji: formData[`${role}_firstNameRomaji`],
+    role: role,
+    jsafId: formData[`${role}_jsafId`],
+    jta: formData[`${role}_jta`],
+    birthDay,
+    sex: formData[`${role}_sex`],
+    address: formData[`${role}_address`],
+    eMail: formData[`${role}_eMail`],
+    phone: formData[`${role}_phone`],
+    fax: formData[`${role}_fax`],
+    miscInJson: "",
+  };
 }
