e3cd88786230613936a2f4be425f01e577fd29a1
Ken Miyashita <kmiyashita23@gmail.com>
2025-01-19 10:57:30 +0900

    feat: react-hook-form を導入してみた
    生の form, input タグではうまく動いているが、MUI と混ぜるとうまくうごかない？

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+
 import * as React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,36 +10,105 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import { Button, Stack } from "@mui/material";
+
 import type { Race } from "@prisma/client";
 
 export type EntryFormProps = {
   raceId: number;
 };
 
+interface EntryFormData {
+  sailNumber: string;
+  country: "JPN" | "USA" | "AUS";
+  boatName?: string;
+  boatWeight: number;
+}
+
+const validationRules = {
+  sailNumber: {
+    required: "セール番号を入力してください",
+    maxLength: 4,
+  },
+  country: {
+    required: "国を選択してください",
+  },
+  boatWeight: {
+    required: "ボート重量を入力してください（キログラた単位）",
+  },
+};
+
 export default function EntryForm({ raceId }: EntryFormProps) {
   const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<EntryFormData>({
+    defaultValues: {
+      country: "JPN",
+    },
+  });
+
+  const onSubmit: SubmitHandler<EntryFormData> = (data) => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("sailNumber", { required: true })} />
+      {errors.sailNumber && <span>セール番号を入力してください</span>}
+      <input type="submit" />
+    </form>
+  );
+  /*
   return (
     <div>
       <h2>エントリー入力</h2>
-      <form>
-        <label>
-          セール番号
-          <input type="text" name="sailNumber" />
-        </label>
-        <label>
-          スキッパー
-          <input type="text" name="skipper" />
-        </label>
-        <label>
-          クルー１
-          <input type="text" name="crew1" />
-        </label>
-        <label>
-          クルー２
-          <input type="text" name="crew2" />
-        </label>
-        <button type="submit">登録</button>
-      </form>
+      <Stack
+        // component={}
+        // noValidate
+        // onSubmit={handleSubmit(onSubmit)}
+        spacing={2}
+        sx={{ m: 2, width: "25ch" }}
+      >
+        <Controller
+          name="sailNumber"
+          control={control}
+          rules={validationRules.sailNumber}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="セール番号"
+              type="text"
+              error={errors.sailNumber !== undefined}
+              helperText={errors.sailNumber?.message}
+            />
+          )}
+        />
+        <Controller
+          name="country"
+          control={control}
+          rules={validationRules.country}
+          render={({ field }) => (
+            <Select
+              {...field}
+              native
+              label="国"
+              error={errors.country !== undefined}
+            >
+              <option value="JPN">日本</option>
+              <option value="USA">アメリカ</option>
+              <option value="AUS">オーストラリア</option>
+            </Select>
+          )}
+        />
+        <Button variant="contained" type="submit">
+          送信する
+        </Button>
+      </Stack>
     </div>
   );
+  */
 }
